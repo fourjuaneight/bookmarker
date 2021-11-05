5abc33e5398037162430ce26f7872fe5ccc46aad
Juan Villela <juan@villela.co>
2021-11-04 23:46:21 -0400

    feat: Add Airtable upload helper.

---
+++ airtable-upload.ts
@@ -0,0 +1,54 @@
+import "https://deno.land/x/dotenv@v3.1.0/load.ts";
+
+import { AirtableError, AirtableResp, BookmarkPodcasts } from "./typings.d.ts";
+
+/**
+ * Upload podcast record object to Airtable
+ * @function
+ * @async
+ *
+ * @param {string} table Airtable table name
+ * @param {BookmarkPodcasts} record formatted podcast record to upload
+ * @return {void}
+ */
+export const airtableUpload = async (
+  table: string,
+  record: BookmarkPodcasts
+): Promise<string> => {
+  const options: RequestInit = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${Deno.env.get("AIRTABLE_API")}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      records: [
+        {
+          fields: {
+            ...record,
+          },
+        },
+      ],
+    }),
+  };
+
+  try {
+    const response: Response = await fetch(
+      `${Deno.env.get("AIRTABLE_BOOKMARKS_ENDPOINT")}/${table}`,
+      options
+    );
+    const results: AirtableResp | AirtableError = await response.json();
+
+    if ((results as AirtableError).errors) {
+      throw new Error(
+        `Uploading repos to Airtable: \n ${
+          (results as AirtableError).errors[0].message
+        }`
+      );
+    }
+
+    return (results as AirtableResp).records[0].fields.title;
+  } catch (error) {
+    throw new Error(`Uploading repos to Airtable: \n ${error}`);
+  }
+};
