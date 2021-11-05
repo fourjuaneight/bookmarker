9b2f90e553c14fcd8e37ba6ed9ffb9eb3f41aae5
Juan Villela <juan@villela.co>
2021-11-05 17:53:39 -0400

    feat: Add articles and comics bookmarking scripts.

---
+++ bookmark-page.ts
@@ -0,0 +1,36 @@
+import { airtableUpload } from "./airtable-upload.ts";
+
+import { BookmarkingResponse } from "./typings.d.ts";
+
+/**
+ * Upload article|comic to Airtable.
+ * @function
+ * @async
+ *
+ * @param {string} table table name
+ * @param {string} title page title
+ * @param {string} creator page creator
+ * @param {string} url page url
+ * @param {string[]} tags record tags
+ * @returns {Promise<BookmarkingResponse>} result of record upload
+ */
+export const bookmarkPage = async (
+  table: string,
+  title: string,
+  creator: string,
+  url: string,
+  tags: string[]
+): Promise<BookmarkingResponse> => {
+  try {
+    const airtableResp = await airtableUpload(table, {
+      title,
+      creator,
+      url,
+      tags,
+    });
+
+    return { success: true, message: airtableResp, source: "bookmarkPage" };
+  } catch (error) {
+    return { success: false, message: error, source: "bookmarkPage" };
+  }
+};
