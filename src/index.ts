4b3f2cbcddea2e28a78d478d5c752641cb76f7f5
Juan Villela <juan@villela.co>
2021-11-08 11:46:34 -0500

    chore: Add event type to request listener.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 import { handleRequest } from './handler';
 
-addEventListener('fetch', event => {
+addEventListener('fetch', (event: FetchEvent) => {
   event.respondWith(handleRequest(event.request));
 });
