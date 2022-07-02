c47b0d72036d65b6b6cd438ef18982356040cf07
Juan Villela <juan@villela.co>
2022-07-02 10:43:31 -0400

    chore: Cleanup logs.

--- src/bookmark-tweets.ts
+++ src/bookmark-tweets.ts
@@ -152,12 +152,10 @@ const getTweetDetails = async (url: string): Promise<TwitterData> => {
       formattedText,
       /(https:\/\/t.co\/[a-zA-z0-9]+)/g
     )
-    console.log({ cleanText });
     const tweet = cleanText
       .replace(/“/g, `"`)
       .replace(/”/g, `"`)
       .replace(/’/g, `'`);
-    console.log({ tweet });
 
     return {
       tweet,
